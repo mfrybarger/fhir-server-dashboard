c9a9cc1631a89a48fd5ed37f09ec268db7aa3504
mfrybarger <mfrybarger@ucsd.edu>
2018-09-07 02:56:26 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -10,7 +10,7 @@ module.exports = {
     // USER SETTINGS
 
     // The URL for the server
-    SERVER: '',
+    SERVER: 'open-ic.epic.com',
 
     // The tags that you wish to include in the Resource Counts Table
     TAGS: [],
